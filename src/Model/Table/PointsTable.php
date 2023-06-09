fead00a667b6e3020b04c9149eaa6b62f123a65a
Marine <marine.marez@gmail.com>
2023-06-09 11:20:35 +0200

    feat: retrave the find on the table

--- src/Model/Table/PointsTable.php
+++ src/Model/Table/PointsTable.php
@@ -15,8 +15,23 @@ class PointsTable extends Table
         'foreignKey' => 'assessment_id',
     ];
 
+    /**
+     * retourn la somme des points bleus et rouges pour 1 vidéo et 1 assessment donnés
+     */
     public function findByVideoAndAssessment($videoId, $assessementId)
     {
-        return ['red' => 123, 'blue' => 456];
+        $query = $this->find('all')
+            ->where([
+                'video_id' => $videoId,
+                'assessment_id' => $assessementId,
+            ]);
+        $result = ['red' => 0, 'blue' => 0];
+        foreach ($query as $point) {
+            if ($point->color_point == "red")
+                $result['red']++;
+            if ($point->color_point == "blue")
+                $result['blue']++;
+        }
+        return $result;
     }
 }
