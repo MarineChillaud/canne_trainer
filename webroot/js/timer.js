f95a07d312a97bbda30ab59fa2ab5210389b3eb4
Marine <marine.marez@gmail.com>
2023-06-30 15:27:23 +0200

    feat: AjaxRequest

--- webroot/js/timer.js
+++ webroot/js/timer.js
@@ -28,14 +28,36 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  let xhr = new XMLHttpRequest();
+
+  function alertContent (){
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.responseText);
+    } else {
+      console.log('Il y a eu un soucis avec la requête');
+    }
+  }
+    function sendAjaxRequest(currentTime, color) {
+    xhr.onreadystatechange = alertContent;
+    xhr.open('POST', 'videos/view/', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('color=' + encodeURIComponent(color) + '&currentTime=' + encodeURIComponent(currentTime));
+  }
+
   redButton.addEventListener('click', function() {
     getCurrentTime();
     console.log('Timing redButton : ', video.currentTime);
+    const redCurrentTime = video.currentTime;
+    const color = 'red'
+    sendAjaxRequest(redCurrentTime, color);
   });
   blueButton.addEventListener('click', function() {
     getCurrentTime();
     console.log('Timing blueButton : ', video.currentTime);
-  });
+    const blueCurrentTime = video.currentTime;
+    const color = 'red'
+    sendAjaxRequest(blueCurrentTime, color);
+    });
 
   // Récupére le currentTime lors du chargement de la vidéo
   video.addEventListener('loadedmetadata', getCurrentTime);
