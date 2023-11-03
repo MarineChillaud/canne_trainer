a0d9a5e7545c31dc3af34ec3f90c6ceafa4aae4c
Marine <marine.marez@gmail.com>
2023-11-03 11:57:16 +0100

    fix: allow public pages for pagescontroller

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Cake\Core\Configure;
+use Cake\Event\EventInterface;
 use Cake\Http\Exception\ForbiddenException;
 use Cake\Http\Exception\NotFoundException;
 use Cake\Http\Response;
@@ -11,6 +12,13 @@ use Cake\View\Exception\MissingTemplateException;
 
 class PagesController extends AppController
 {
+    public function beforeFilter(EventInterface $event)
+    {
+        parent::beforeFilter($event);
+
+        $this->Authentication->allowUnauthenticated('*');
+    }
+
     public function display(string ...$path): ?Response
     {
         if (!$path) {
