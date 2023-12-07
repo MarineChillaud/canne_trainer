054d85f1dfbfd44f2d92e28575a1f58ff734ec35
Marine <marine.marez@gmail.com>
2023-12-07 16:31:18 +0100

    change button places around video

--- templates/Videos/view.php
+++ templates/Videos/view.php
@@ -14,45 +14,53 @@
 
 <div class="container text-center">
     <h1 class='mb-5'><?= h($video->title) ?></h1>
-    <!-- Problèmes autoplay/CSS responsive -->
-    <!-- <div class="row align-items-center">
-        <video id="video" autoplay='true' src="https://canne.tv/replay/video/3513/A/2022-01-21_10-20-00___2022-01-21_10-34-18.mp4" type="video/mp4" controls style='width:100%'></video>
-    </div> -->
 
     <div class="row align-items-center">
-        <video id="video" autoplay controls data-next-video="<?= $this->Url->build(['controller' => 'Assessments', 'action' => 'review', $video->id, $assessmentId]); ?>">
-            <source src="https://canne.tv/replay/video/3513/A/2022-01-21_10-20-00___2022-01-21_10-34-18.mp4" type="video/mp4" style='width:100%' />
-        </video>
+        <div class="col-md-3">
+            <div class="points-button row align-items-center">
+                <div class="col align-self-center">
+                    <div type="button" class="btn btn-danger btn-lg">
+                        <?= $this->Form->create(null, ['id' => 'point_form_red']) ?>
+                        <?= $this->Form->hidden('color_point', ['value' => 'red']) ?>
+                        <?= $this->Form->hidden('video_id', ['value' => $video->id]) ?>
+                        <?= $this->Form->hidden('current_time', ['id' => 'current_time_red']); ?>
+                        <?= $this->Form->button($points['red'], ['id' => 'redButton', 'class' => 'btn btn-danger']) ?>
+                        <?= $this->Form->hidden('_csrfToken', ['value' => $this->request->getAttribute('csrfToken')]); ?>
+                        <?= $this->Form->end() ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="row align-items-center">
+                <video id="video" autoplay=true controls data-next-video="<?= $this->Url->build(['controller' => 'Assessments', 'action' => 'review', $video->id, $assessmentId]); ?>">
+                    <source src="https://canne.tv/replay/video/3513/A/2022-01-21_10-20-00___2022-01-21_10-34-18.mp4" type="video/mp4" style='width:100%' />
+                </video>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="points-button row align-items-center">
+                <div class="col align-self-center">
+                    <div type="button" class="btn btn-primary btn-lg">
+                        <?= $this->Form->create(null, ['id' => 'point_form_blue']) ?>
+                        <?= $this->Form->hidden('color_point', ['value' => 'blue']) ?>
+                        <?= $this->Form->hidden('video_id', ['value' => $video->id]) ?>
+                        <?= $this->Form->hidden('current_time', ['id' => 'current_time_blue']); ?>
+                        <?= $this->Form->button($points['blue'], ['id' => 'blueButton', 'class' => 'btn btn-primary']) ?>
+                        <?= $this->Form->hidden('_csrfToken', ['value' => $this->request->getAttribute('csrfToken')]); ?>
+                        <?= $this->Form->end() ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div id="points-button" class="row align-items-center">
-        <div class="col align-self-center">
-            <div type="button" class="btn btn-danger btn-lg">
-                <?= $this->Form->create(null, ['id' => 'point_form_red']) ?>
-                <?= $this->Form->hidden('color_point', ['value' => 'red']) ?>
-                <?= $this->Form->hidden('video_id', ['value' => $video->id]) ?>
-                <?= $this->Form->hidden('current_time', ['id' => 'current_time_red']); ?>
-                <?= $this->Form->button($points['red'], ['id' => 'redButton', 'class' => 'btn btn-danger']) ?>
-                <?= $this->Form->hidden('_csrfToken', ['value' => $this->request->getAttribute('csrfToken')]); ?>
-                <?= $this->Form->end() ?>
-            </div>
-        </div>
-        <div class="col align-self-center">
-            <div type="button" class="btn btn-primary btn-lg">
-                <?= $this->Form->create(null, ['id' => 'point_form_blue']) ?>
-                <?= $this->Form->hidden('color_point', ['value' => 'blue']) ?>
-                <?= $this->Form->hidden('video_id', ['value' => $video->id]) ?>
-                <?= $this->Form->hidden('current_time', ['id' => 'current_time_blue']); ?>
-                <?= $this->Form->button($points['blue'], ['id' => 'blueButton', 'class' => 'btn btn-primary']) ?>
-                <?= $this->Form->hidden('_csrfToken', ['value' => $this->request->getAttribute('csrfToken')]); ?>
-                <?= $this->Form->end() ?>
-            </div>
-        </div>
-    </div>
+    
     <div>
         <div id="flagContainer"></div>
         <div id="timeProgress" class="progress bg-secondary mx-auto mt-4"">
-            <div id="progressBar" class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+        <div id="progressBar" class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
-</div>
 </div>
